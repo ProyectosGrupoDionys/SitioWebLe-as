a25e18483339312663854e3c14d4b9b2590bb42a
DiegoSTI <soporteti2@grupodionys.com.pe>
2017-11-16 15:26:18 -0500

    subida de archivos de la pagina web leñas

---
+++ js/php-servicios/servicios-camping.php
@@ -0,0 +1,12 @@
+<?php  
+	$destino= "soporteti@grupodionys.com.pe";
+	$nombre=$_POST["nombre"];
+	$email=$_POST["email"];
+	$telefono=$_POST["telefono"];
+	$correo=$_POST["email"];
+	$mensaje=$_POST["mensaje"];
+	$contenido = "Nombre: " . $nombre .  "\nEmail: " . $email . "\nTelefono: " . $telefono . "\nCorreo: " . $correo ."\nMensaje: " . $mensaje;
+
+	mail($destino, "Web Las Leñas", $contenido);
+	header("Location:../servicio-camping.html");
+ ?>
